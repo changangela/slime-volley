f84c716d490c7bfa868c899b9be0aa725bde979e
wendydeng <wendy9852@hotmail.com>
2017-03-12 00:49:10 -0500

    updated slime

--- js/slime.js
+++ js/slime.js
@@ -3,7 +3,10 @@ function Slime(canvasX, canvasY, color){
 	this.color = color;
 	this.position = new p5.Vector(canvasX/4, canvasY);
 	this.eyePosition = new p5.Vector(this.position.x + this.radius/2, this.position.y - this.radius/2);
+	this.eyeballPosition = new p5.Vector(0, 0);
+	//this.showEyeBall = function(ballPosition){
 
+	//}
 	this.show = function(){
 		fill(this.color);
 		fill("color");
