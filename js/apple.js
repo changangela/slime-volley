1d3ef2109cc562a5730e92c8eef5848b995fc713
changangela <angelachang27@gmail.com>
2017-03-11 23:38:21 -0500

    created sketch file

--- js/apple.js
+++
@@ -1,8 +0,0 @@
-function Apple() {
-    this.position = randomPosition();
-
-    this.show = function() {
-    	fill("red");
-        rect(this.position.x * GRID_SIZE, this.position.y * GRID_SIZE, GRID_SIZE, GRID_SIZE);
-    }
-}
