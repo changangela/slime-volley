ac29c7c23453795b99e9c4162d0ee3c62ab1b363
wendydeng <wendy9852@hotmail.com>
2017-03-12 00:45:28 -0500

    drew slime

--- js/slime.js
+++ js/slime.js
@@ -1,20 +1,21 @@
 function Slime(canvasX, canvasY, color){
-	this.radius = 30;
+	this.radius = 100;
 	this.color = color;
 	this.position = new p5.Vector(canvasX/4, canvasY);
 	this.eyePosition = new p5.Vector(this.position.x + this.radius/2, this.position.y - this.radius/2);
+
 	this.show = function(){
 		fill(this.color);
-		fill("black");
+		fill("color");
 		//rect(0,0,50,50);
 
 		console.log(Math.PI);
-		arc(50, 50, 50, 50, 0, PI);
 
-		arc(this.position.x, this.position.y, this.radius, this.radius, 0, PI);
-		//arc(this.eyePosition.x, this.eyePosition.y, this.radius/4, 0, Math.PI*2);
+		arc(this.position.x, this.position.y, 2*this.radius, 2*this.radius, 0, 0, PI);
+		fill("white");
+		arc(this.eyePosition.x, this.eyePosition.y, this.radius/2, this.radius/2, 0, Math.PI*2);
 		
-		text("HIIIIIIIIIIIIIIIIIIIIIIIIIIIII", 50, 50);
+		
 	}
 
 }
