9099789f55c29ea1e9e43902a938a324fa69f608
changangela <angelachang27@gmail.com>
2017-03-12 00:00:54 -0500

    created sketch file setup and draw

---
+++ js/sketch.js
@@ -0,0 +1,16 @@
+var CANVAS_X = 1000;
+var CANVAS_Y = 750;
+var FRAME_RATE = 10;
+var BACKGROUND_COLOR = "#ADD8E6"; // light blue
+
+var GROUND_COLOR = "#99ff66"; // light green
+var GROUND_HEIGHT = 20;
+
+function setup() {
+    createCanvas(CANVAS_X, CANVAS_Y);
+    frameRate(FRAME_RATE);
+
+}
+
+function draw() {
+}
