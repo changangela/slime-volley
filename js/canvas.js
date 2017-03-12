ef9f3e9f4fedd23e2afd744bcc1500733e8bd746
changangela <angelachang27@gmail.com>
2017-03-12 00:51:13 -0500

    resolving merge

--- js/canvas.js
+++ js/canvas.js
@@ -1,6 +1,5 @@
 function Canvas(canvasX, canvasY, canvasColor) {
-    this.sizeX = canvasX;
-    this.sizeY = canvasY;
+    this.size = new p5.Vector(canvasX, canvasY);
     this.color = canvasColor;
 
     this.show = function() {
